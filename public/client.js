defd3c63d9529f90db6e143cdad6391d05129747
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2022-11-13 19:26:42 +0100

    (Trello: Client) No longer init after timeout. Unneeded after correcting global script

--- public/client.js
+++ public/client.js
@@ -1,15 +1,13 @@
 
-setTimeout(() => {
-	window.TrelloPowerUp.initialize({
-		"card-back-section": (t) => {
-			return {
-				title: "My Back Section",
-				icon: "/favicon.ico",
-				content: {
-					type: "iframe",
-					url: t.signUrl("./card-back-section"),
-				},
-			};
-		}
-	});
-}, 500);
+window.TrelloPowerUp.initialize({
+	"card-back-section": (t) => {
+		return {
+			title: "My Back Section",
+			icon: "/favicon.ico",
+			content: {
+				type: "iframe",
+				url: t.signUrl("./card-back-section"),
+			},
+		};
+	}
+});
--- src/pages/card-back-section.tsx
+++ src/pages/card-back-section.tsx
@@ -1,12 +1,15 @@
 import Background from "../components/background";
 import Shield from "../components/shield";
 
+const t = globalThis.window?.TrelloPowerUp?.iframe();
 
 export default function cardBackSection() {
+	const card = t?.card("id");
+	card?.then(console.log);
 	return (
 		<Background
 			pageHead={{
-				title: 'JmBoost: Card Back Section'
+				title: "JmBoost: Card Back Section"
 			}}
 		>
 			<Shield
