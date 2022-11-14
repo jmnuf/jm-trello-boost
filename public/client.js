f269ffb8b0233a8265249409e860888d4d11339a
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2022-11-14 01:37:37 +0100

    (Trello API) Added card badges to capabilities

--- public/client.js
+++ public/client.js
@@ -6,12 +6,25 @@ window.TrelloPowerUp.initialize({
 	"card-back-section": (t) => {
 		return {
 			title: "My Back Section",
-			icon: "/favicon.ico",
+			icon: "https://img.shields.io/badge/jm-priority-blueviolet?style=plastic",
 			content: {
 				type: "iframe",
 				url: t.signUrl("./card-back-section"),
 				height: 180,
 			},
 		};
+	},
+	"card-badges": (t) => {
+		return t.get("card", "shared", "priority").then(value => {
+			if (typeof value != "number") {
+				throw t.NotHandled();
+			}
+			return [
+				{
+					title: "Priority",
+					icon: "https://img.shields.io/badge/priority-" + String(value) + "-blueviolet?style=social",
+				}
+			]
+		})
 	}
 });
